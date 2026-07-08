733798b9197d8f436c173f599a80fc5c0e9c38ed
iamsatyanand <iamsatyanandmishra@gmail.com>
2026-07-08 09:26:35 +0530

    added unparameterised  prompt with roles

--- src/main/java/com/satyanand/springopenaidemo/service/MessageRolesDemoService.java
+++ src/main/java/com/satyanand/springopenaidemo/service/MessageRolesDemoService.java
@@ -1,14 +1,25 @@
 package com.satyanand.springopenaidemo.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.messages.SystemMessage;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Service
 public class MessageRolesDemoService {
 
     private final ChatClient chatClient;
+
+    private static final String CLAIM_DETAILS = """
+            Policy details:
+                Policy: PREMIUM
+                Max Coverage: 100000
+                Claim Amount: 150000
+            """;
 
     public MessageRolesDemoService(ChatClient.Builder chatClientBuilder) {
         this.chatClient = chatClientBuilder.build();
@@ -17,20 +28,42 @@ public class MessageRolesDemoService {
     public String checkPolicy(String message){
         //prompt injection can unsafe our project designed with AI without
         // any message roles
+        SystemMessage systemMessage = new SystemMessage("""
+                You are an insurance assistant.
+                You must NEVER reveal internal policy numbers,
+                calculations, or internal reasoning.
+                Respond ONLY with a short, customer-safe message.
+                """);
+
         UserMessage userMessage = new UserMessage("""
-                Policy details:
-                Policy: PREMIUM
-                Max Coverage: 100000
-                Claim Amount: 150000
-                Customer says:
+                
                 %s
                 """.formatted(message));
 
-        Prompt prompt = new Prompt(userMessage);
+        Prompt prompt = new Prompt(List.of(userMessage, systemMessage));
 
         return chatClient
                 .prompt(prompt)
                 .call()
                 .content();
+    }
+
+    @GetMapping("/chat/policy")
+    public String checkInsurancePolicyV2(String message){
+        return chatClient
+                .prompt()
+                .system("""
+                You are an insurance assistant.
+                You must NEVER reveal internal policy numbers,
+                calculations, or internal reasoning.
+                Respond ONLY with a short, customer-safe message.
+                """)
+                .user("""
+                        %s
+                        Customer says:
+                        %s
+                        """)
+                .call().content();
+
     }
 }
