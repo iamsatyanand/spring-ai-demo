4a4ac591bc049082bc12dc05600b8f8bcdf62ade
iamsatyanand <iamsatyanandmishra@gmail.com>
2026-07-19 17:23:20 +0530

    implemented prompt stuffing with dynamic string template

---
+++ src/main/java/com/satyanand/springopenaidemo/controller/OrderSupportAIAssistantController.java
@@ -0,0 +1,24 @@
+package com.satyanand.springopenaidemo.controller;
+
+import com.satyanand.springopenaidemo.service.OrderSupportAIAssistantService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class OrderSupportAIAssistantController {
+
+    private OrderSupportAIAssistantService aiAssistantService;
+
+    public OrderSupportAIAssistantController(OrderSupportAIAssistantService aiAssistantService) {
+        this.aiAssistantService = aiAssistantService;
+    }
+
+    @GetMapping("/order-support")
+    public String getOrderSupportResponse(@RequestParam String customerName, @RequestParam String orderId, @RequestParam String customerMessage) {
+        return aiAssistantService.assistWithOrderSupport(customerName, orderId, customerMessage);
+    }
+
+}
