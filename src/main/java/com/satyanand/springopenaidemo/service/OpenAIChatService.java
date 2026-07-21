261109d85a2385a6ed95411120c0f440f387a02f
iamsatyanand <iamsatyanandmishra@gmail.com>
2026-07-21 22:23:05 +0530

    added chat options with different options

--- src/main/java/com/satyanand/springopenaidemo/service/OpenAIChatService.java
+++ src/main/java/com/satyanand/springopenaidemo/service/OpenAIChatService.java
@@ -1,7 +1,10 @@
 package com.satyanand.springopenaidemo.service;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class OpenAIChatService {
@@ -13,8 +16,22 @@ public class OpenAIChatService {
     }
 
     public String chatWithOpenAiLLM(String message){
+
+        ChatOptions chatOptions = ChatOptions.builder()
+//                .model("gpt-4o-mini")
+                .model("gemini-3.5-flash")
+                .temperature(0.3)
+                .maxTokens(100)
+//                .frequencyPenalty(0.7)
+//                .presencePenalty(0.7)
+                .stopSequences(List.of("}"))
+                .topK(50)
+                .topP(0.5)
+                .build();
+
         return chatClient
                 .prompt(message)
+                .options(chatOptions.mutate())
                 .call()
                 .content();
     }
